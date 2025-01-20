a616ba3b2ecbf3fc060c417b277eddc2e6a2c719
Ramchandra Reddy Sathu <ramchandrareddy704@gmail.com>
2025-01-20 12:18:49 -0600

    Change sto timetable

--- Unit-1/timetabled/src/calendarcomp/Calender.jsx
+++ Unit-1/timetabled/src/calendarcomp/Calender.jsx
@@ -39,6 +39,92 @@ const Calender = () => {
             <td></td>
             <EventMod event="The Bean 🫘" color="blue" />
           </tr>
+          <tr>
+            <td className="time">10 am</td>
+            <td></td>
+            <EventMod event="River Cruise ⛵️" color="blue" />
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">11 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <EventMod event="Deep Dish 🍕" color="green" />
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">12 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <EventMod event="Subway 🚊" color="pink" />
+            <td></td>
+           
+          </tr>
+
+          <tr>
+            <td className="time">1 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">2 pm</td>
+            <td></td>
+            <td></td>
+            <EventMod event="Art Institute 🖼" color="blue" />
+            <td></td>
+            <EventMod event="Girl & the Goat" color="green" />
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">3 pm</td>
+            <EventMod event="Cubs Game ⚾️" color="blue" />
+            <td></td>
+            <td></td>
+            <td></td>
+            <EventMod event="Subway 🚊" color="pink" />
+            <td></td>
+            <td></td>
+          </tr>
+
+          <tr>
+            <td className="time">4 pm</td>
+            <td></td>
+            <td></td>
+            <EventMod event="Fancy Dinner 🎩" color="green" />
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>  
+          </tr>
+
+          <tr>
+            <td className="time">5 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <EventMod event="Shopping 🛍" color="blue" />
+            <td></td>
+            
+          </tr>
           {/* Additional rows for other weeks */}
         </tbody>
       </table>
